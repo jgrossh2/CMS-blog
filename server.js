91b472cf78bae24ab84fe62bc49ec977e50d7948
Mona Lisa <joannagrosshans2@gmail.com>
2020-12-07 18:53:39 -0600

    files set up

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql2');
+const sequelize = require('./config/cnnection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    //actualy secret would be stored in .env file
+    secret: 'Super secret secret',
+    //empty brackets is all that is needed
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+//gives the url translated to something useable
+app.use(express.urlencoded({ extended: true }));
+//takes contents of folder as static assets
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+//sync is sequelize taking models and connecting them to database tables
+//force: true = drop and re-create tables on startup (instead of DROP TABLE IF EXISTS)
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
